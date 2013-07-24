989da64a0cd15ac6f4635715f3955fd3b5c03820
Shin Ho <shin@tnwlabs.com>
2013-07-24 13:11:22 +0200

    First commit
    Default Laravel workbench.

---
+++ src/Shincode/Envision/EnvisionServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Shincode\Envision;
+
+use Illuminate\Support\ServiceProvider;
+
+class EnvisionServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('shincode/envision');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
