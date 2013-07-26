b1fe2287c3ce0e2193ffa5bb38f031f26c9cf9bd
Shin Ho <shin@shincode.com>
2013-07-26 18:09:36 +0200

    Added Controller to autoload

--- src/Shincode/Envision/Support/Autoloader.php
+++ src/Shincode/Envision/Support/Autoloader.php
@@ -22,34 +22,42 @@ class Autoloader {
         static::$templatesDir = __DIR__.DIRECTORY_SEPARATOR.'Templates'.DIRECTORY_SEPARATOR;
         static::$autoGenerate = $create;
         spl_autoload_register(array($this, 'resource'));
+        spl_autoload_register(array($this, 'controller'));
+    }
+
+    public function controller($class) {
+
+        $path = app_path().DIRECTORY_SEPARATOR.'controllers'.DIRECTORY_SEPARATOR;
+        $namespace = null;
+        $this->check($path, $namespace, $class, ucfirst(__FUNCTION__));
+
     }
 
     public function resource($class) {
         
-        $this->check($class, ucfirst(__FUNCTION__));
+        $path = app_path().DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.str_plural(strtolower(__FUNCTION__)).DIRECTORY_SEPARATOR;
+        $namespace = str_plural(ucfirst(__FUNCTION__));
+        $this->check($path, $namespace, $class, ucfirst(__FUNCTION__));
 
     }
 
     // Check if anything could be executed
-    protected function check($class, $parent) {
+    protected function check($path, $namespace, $class, $parent) {
         // Continue if classname check passes
         if (substr($class, -strlen($parent)) == $parent) {
 
             // Create vars
-            $parentnamespace = 'Shincode\\Envision\\Support';
-            $namespace = str_plural($parent);
-            $path = app_path().DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.str_plural($parent).DIRECTORY_SEPARATOR;
             $filename = $class.'.php';
 
             // Create the class if needed
             if (static::$autoGenerate) {
-                $this->create($path, $class, $filename, $parent, $parentnamespace, $namespace);
+                $this->create($path, $class, $filename, $parent, $namespace);
             } else {
                 // WIll not generate, but at least check if the file exists. If not, return.
                 if (!File::exists($path.$filename))
                     return;
             }
-            
+
             // Load the class
             $this->load($path, $class, $namespace);
             
@@ -59,7 +67,7 @@ class Autoloader {
     }
 
     // Create the necessary files
-    protected function create($path, $class, $filename, $parent, $parentnamespace, $namespace) {
+    protected function create($path, $class, $filename, $parent, $namespace) {
         // Check if directory exists
         if (!File::exists($path)) {
             File::makeDirectory($path);
@@ -73,9 +81,10 @@ class Autoloader {
             $location = File::exists($location) ? $location : static::$templatesDir.'Default.txt';
 
             $code = File::get($location);
+            $filenamespace = ($namespace) ? 'namespace '.$namespace.';' : '';
 
-            $originaltags = array('{{ parentnamespace }}', '{{ namespace }}', '{{ class }}', '{{ parent }}');
-            $replacewith = array($parentnamespace, $namespace, $class, $parent);
+            $originaltags = array('{{ parentnamespace }}', '{{ filenamespace }}', '{{ class }}', '{{ parent }}');
+            $replacewith = array(__NAMESPACE__, $filenamespace, $class, $parent);
 
             $code = str_replace($originaltags, $replacewith, $code);
 
@@ -87,10 +96,13 @@ class Autoloader {
 
         require($path.$class.'.php');
 
+        $namespace .= ($namespace) ? '\\' : '';
+
         // Create Alias
         $loader = AliasLoader::getInstance();
-        $loader->alias($class, $namespace.'\\'.$class);
-        $loader->load($class);
+        $loader->alias($class, $namespace.$class);
+        if (!class_exists($class))
+            $loader->load($class);
 
     }
 
---
+++ src/Shincode/Envision/Support/Controller.php
@@ -0,0 +1,11 @@
+<?php namespace Shincode\Envision\Support;
+
+use \BaseController;
+
+abstract class Controller extends BaseController {
+
+	public function getIndex() {
+        //
+    }
+
+}
